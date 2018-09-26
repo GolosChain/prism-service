26e7c351503578f6d79b99f992a99c91c6cbf272
Oleg <oleg.pav.m@gmail.com>
2018-09-26 15:11:51 +0300

    Extractor draft.

--- src/services/Extractor.js
+++ src/services/Extractor.js
@@ -6,16 +6,56 @@ const BlockSubscribe = core.services.BlockSubscribe;
 const BlockSubscribeRestore = core.services.BlockSubscribeRestore;
 const env = require('../env');
 
-class Cleaner extends BasicService {
+class Extractor extends BasicService {
+    constructor() {
+        super();
+
+        this._blockQueue = [];
+        this._subscribe = new BlockSubscribe();
+        this.addNested(this._subscribe);
+    }
+
     async start() {
-        // TODO -
+        await this.restore();
+        await this._subscribe.start((block, blockNum) => {
+            this._blockQueue.push([block, blockNum]);
+        });
+
+        this._runIterator().catch((error) => {
+            stats.increment('extractor_iteration_error');
+            Logger.error(`Extractor iteration error - ${error}`);
+            process.exit(1);
+        });
     }
 
     async stop() {
-        // TODO -
+        await this.stopNested();
     }
 
     async restore() {
+        // TODO -
+    }
+
+    async _runIterator() {
+        while (true) {
+            await this._tick();
+            await new Promise((resolve) => {
+                setImmediate(resolve);
+            });
+        }
+    }
+
+    async _tick() {
+        let blockData;
+
+        while (blockData = this._blockQueue.shift()) {
+            const [block, blockNum] = blockData;
+
+            await this._handleBlock(block, blockNum);
+        }
+    }
+
+    async _handleBlock(block, blockNum) {
         // TODO -
     }
 }
