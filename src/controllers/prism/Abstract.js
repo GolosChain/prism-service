2ca7e5ee9e19a2752be2580d9066ef862e9ccfb6
Oleg <oleg.pav.m@gmail.com>
2018-10-01 18:22:46 +0300

    Abstract prism + abstract comment.

--- src/controllers/prism/Abstract.js
+++ src/controllers/prism/Abstract.js
@@ -1,6 +1,16 @@
 class Abstract {
-    static async handle(data) {
+    async handle(data) {
         throw 'Handler not implemented';
+    }
+
+    async _getOrCreateModel(modelClass, queryForCheck) {
+        let model = await modelClass.findOne(queryForCheck);
+
+        if (!model) {
+            model = new modelClass();
+        }
+
+        return model;
     }
 }
 
