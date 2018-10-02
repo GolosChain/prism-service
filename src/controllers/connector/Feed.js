47cd62bd9b1489edcad22388b88f1c9fb4859927
Oleg <oleg.pav.m@gmail.com>
2018-10-02 15:51:34 +0300

    Filtrate feed by tags + stubs.

--- src/controllers/connector/Feed.js
+++ src/controllers/connector/Feed.js
@@ -3,32 +3,46 @@ const Abstract = require('./Abstract');
 const Post = require('../../models/Post');
 
 class Feed extends Abstract {
-    async handle({ type, fromId = null, limit = 20 }) {
+    async handle({ type, tags = [], fromId = null, limit = 20 }) {
+        [limit, tags] = this._normalizeRequestParams(limit, tags);
+
+        switch (type) {
+            case 'natural':
+                return await this._getNaturalFeed(fromId, limit, tags);
+            case 'popular':
+                return await this._getPopularFeed(fromId, limit, tags);
+            case 'actual':
+                return await this._getActualFeed(fromId, limit, tags);
+            case 'promo':
+                return await this._getPromoFeed(fromId, limit, tags);
+            default:
+                throw { code: 12001, message: 'Invalid feed type' };
+        }
+    }
+
+    _normalizeRequestParams(limit, tags) {
         limit = +limit;
 
         if (!limit || limit <= 0 || limit > env.GLS_MAX_FEED_LIMIT) {
             limit = 20;
         }
 
-        switch (type) {
-            case 'natural':
-                return await this._getNaturalFeed(fromId, limit);
-            case 'popular':
-                return await this._getPopularFeed(fromId, limit);
-            case 'actual':
-                return await this._getActualFeed(fromId, limit);
-            case 'promo':
-                return await this._getPromoFeed(fromId, limit);
-            default:
-                throw { code: 12001, message: 'Invalid feed type' };
+        if (!Array.isArray(tags)) {
+            tags = [];
         }
+
+        return [limit, tags];
     }
 
-    async _getNaturalFeed(fromId, limit) {
-        let query = {};
+    async _getNaturalFeed(fromId, limit, tags) {
+        const query = {};
 
         if (fromId) {
-            query = { $lt: fromId };
+            query._id = { $lt: fromId };
+        }
+
+        if (tags.length) {
+            query['metadata.tags'] = { $in: tags };
         }
 
         let data = await Post.find(query, { __v: false }, { limit, lean: true, sort: { _id: -1 } });
@@ -43,15 +57,15 @@ class Feed extends Abstract {
         };
     }
 
-    _getPopularFeed(fromId, limit) {
+    _getPopularFeed(fromId, limit, tags) {
         // TODO -
     }
 
-    _getActualFeed(fromId, limit) {
+    _getActualFeed(fromId, limit, tags) {
         // TODO -
     }
 
-    _getPromoFeed(fromId, limit) {
+    _getPromoFeed(fromId, limit, tags) {
         // TODO -
     }
 }
---
+++ src/controllers/connector/Post.js
@@ -0,0 +1,10 @@
+const env = require('../../data/env');
+const Abstract = require('./Abstract');
+
+class Post extends Abstract {
+    async handle() {
+        // TODO -
+    }
+}
+
+module.exports = Post;
