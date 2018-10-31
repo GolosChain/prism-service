35e583ba79572a8b1730ddb01caaf71505e119e6
Oleg <oleg.pav.m@gmail.com>
2018-10-31 17:58:11 +0300

    #21, #22 Cache blockchain props and feed price.

--- src/data/env.js
+++ src/data/env.js
@@ -3,4 +3,6 @@ const env = process.env;
 
 module.exports = {
     GLS_MAX_FEED_LIMIT: env.GLS_MAX_FEED_LIMIT || 100,
+    GLS_CHAIN_PROPS_INTERVAL: env.GLS_CHAIN_PROPS_INTERVAL || 60000,
+    GLS_FEED_PRICE_INTERVAL: env.GLS_FEED_PRICE_INTERVAL || 60000,
 };
