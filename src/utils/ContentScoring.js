7aca87721c723c5eb526888459dd35d936d781c3
Oleg <oleg.pav.m@gmail.com>
2018-11-01 14:56:20 +0300

    #5, #6 Content scoring.

---
+++ src/utils/ContentScoring.js
@@ -0,0 +1,47 @@
+const core = require('gls-core-service');
+const BigNum = core.types.BigNum;
+const BigNumUtils = BigNum.OriginalBigNumber();
+
+const HOT_RSHARES_BASE = 10000000;
+const HOT_TIME_DEGRADATION = 10000;
+const TRENDING_RSHARES_BASE = 10000000;
+const TRENDING_TIME_DEGRADATION = 480000;
+
+class ContentScoring {
+    calcHot(netRshares, createdInBlockChain) {
+        return this._calcLikeAWilson({
+            HOT_RSHARES_BASE,
+            HOT_TIME_DEGRADATION,
+            netRshares,
+            createdInBlockChain,
+        });
+    }
+
+    calcTrending(netRshares, createdInBlockChain) {
+        return this._calcLikeAWilson({
+            TRENDING_RSHARES_BASE,
+            TRENDING_TIME_DEGRADATION,
+            netRshares,
+            createdInBlockChain,
+        });
+    }
+
+    _calcLikeAWilson({ rsharesBase, timeDegradation, netRshares, createdInBlockChain }) {
+        const seconds = +createdInBlockChain / 1000;
+        const modRshares = netRshares.div(rsharesBase);
+        const order = Math.log10(BigNumUtils.max(modRshares.abs(), 1));
+        let sign;
+
+        if (modRshares.gt(0)) {
+            sign = 1;
+        } else if (modRshares.lt(0)) {
+            sign = -1;
+        } else {
+            sign = 0;
+        }
+
+        return order * sign + seconds / timeDegradation;
+    }
+}
+
+module.exports = ContentScoring;
