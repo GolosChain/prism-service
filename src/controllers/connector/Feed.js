4458b6b347352fc3c78449462727c903f5be4fd1
Oleg <oleg.pav.m@gmail.com>
2018-12-12 19:53:35 +0300

    #53 Split feed api.

--- src/controllers/connector/Feed.js
+++ src/controllers/connector/Feed.js
@@ -4,27 +4,38 @@ const Post = require('../../models/Post');
 const User = require('../../models/User');
 
 class Feed extends Abstract {
-    async handle({ user, type, tags = [], fromId = null, limit = 20 }) {
+    async handleNatural({ tags = [], afterId = null, limit = 20 }) {
         [limit, tags] = this._normalizeRequestParams(limit, tags);
 
-        switch (type) {
-            case 'natural':
-                return await this._getNaturalFeed({ fromId, limit, tags });
-            case 'popular':
-                return await this._getPopularFeed({ fromId, limit, tags });
-            case 'actual':
-                return await this._getActualFeed({ fromId, limit, tags });
-            case 'promo':
-                return await this._getPromoFeed({ fromId, limit, tags });
-            case 'personal':
-                return await this._getPersonalFeed({ fromId, limit, tags, user });
-            default:
-                throw { code: 12001, message: 'Invalid feed type' };
-        }
+        return await this._getNaturalFeed({ afterId, limit, tags });
+    }
+
+    async handlePopular({ tags = [], afterId = null, limit = 20 }) {
+        [limit, tags] = this._normalizeRequestParams(limit, tags);
+
+        return await this._getPopularFeed({ afterId, limit, tags });
+    }
+
+    async handleActual({ tags = [], afterId = null, limit = 20 }) {
+        [limit, tags] = this._normalizeRequestParams(limit, tags);
+
+        return await this._getActualFeed({ afterId, limit, tags });
+    }
+
+    async handlePromo({ tags = [], afterId = null, limit = 20 }) {
+        [limit, tags] = this._normalizeRequestParams(limit, tags);
+
+        return await this._getPromoFeed({ afterId, limit, tags });
+    }
+
+    async handlePersonal({ user, tags = [], afterId = null, limit = 20 }) {
+        [limit, tags] = this._normalizeRequestParams(limit, tags);
+
+        return await this._getPersonalFeed({ afterId, limit, tags, user });
     }
 
     _normalizeRequestParams(limit, tags) {
-        limit = +limit;
+        limit = Number(limit);
 
         if (!limit || limit <= 0 || limit > env.GLS_MAX_FEED_LIMIT) {
             limit = 20;
@@ -37,43 +48,43 @@ class Feed extends Abstract {
         return [limit, tags];
     }
 
-    async _getNaturalFeed({ fromId, limit, tags }) {
+    async _getNaturalFeed({ afterId, limit, tags }) {
         const query = {};
 
-        this._injectQueryParams(query, { fromId, tags });
+        this._injectQueryParams(query, { afterId, tags });
 
         return await this._queryFeedWithAnnotate(query, { _id: -1 }, limit);
     }
 
-    async _getPopularFeed({ fromId, limit, tags }) {
+    async _getPopularFeed({ afterId, limit, tags }) {
         const query = {};
 
-        this._injectQueryParams(query, { fromId, tags });
+        this._injectQueryParams(query, { afterId, tags });
 
         return await this._queryFeedWithAnnotate(query, { 'scoring.popular': -1 }, limit);
     }
 
-    async _getActualFeed({ fromId, limit, tags }) {
+    async _getActualFeed({ afterId, limit, tags }) {
         const query = {};
 
-        this._injectQueryParams(query, { fromId, tags });
+        this._injectQueryParams(query, { afterId, tags });
 
         return await this._queryFeedWithAnnotate(query, { 'scoring.actual': -1 }, limit);
     }
 
-    async _getPromoFeed({ fromId, limit, tags }) {
+    async _getPromoFeed({ afterId, limit, tags }) {
         const payoutDate = new Date(Date.now() - env.GLS_PAYOUT_RANGE);
         const query = { createdInBlockchain: { $gt: payoutDate } };
 
-        this._injectQueryParams(query, { fromId, tags });
+        this._injectQueryParams(query, { afterId, tags });
 
         return await this._queryFeedWithAnnotate(query, { 'promote.balance': -1 }, limit);
     }
 
-    async _getPersonalFeed({ fromId, limit, user, tags }) {
+    async _getPersonalFeed({ afterId, limit, user, tags }) {
         const query = {};
 
-        this._injectQueryParams(query, { fromId, tags });
+        this._injectQueryParams(query, { afterId, tags });
 
         const userModel = await User.findOne({ name: user }, { following: true });
 
@@ -86,9 +97,9 @@ class Feed extends Abstract {
         return await this._queryFeedWithAnnotate(query, { _id: -1 }, limit);
     }
 
-    _injectQueryParams(query, { fromId, tags }) {
-        if (fromId) {
-            query._id = { $lt: fromId };
+    _injectQueryParams(query, { afterId, tags }) {
+        if (afterId) {
+            query._id = { $lt: afterId };
         }
 
         if (tags.length) {
