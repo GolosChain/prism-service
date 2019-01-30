d9097d6d5ee86e052a082e49896b7ba855ddbf20
Oleg <oleg.pav.m@gmail.com>
2019-01-30 19:32:31 +0300

    #72, #73 Get comments controller stub.

---
+++ src/controllers/connector/Comment.js
@@ -0,0 +1,10 @@
+const core = require('gls-core-service');
+const BasicController = core.controllers.Basic;
+
+class Comment extends BasicController {
+    async getComments({}) {
+        // TODO -
+    }
+}
+
+module.exports = Comment;
