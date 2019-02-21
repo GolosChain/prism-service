8a89cd855ec71029e2b1736277613b97fff291ca
Oleg <oleg.pav.m@gmail.com>
2019-02-21 20:10:27 +0300

    #69 Content extraction.

--- src/controllers/prism/AbstractContent.js
+++ src/controllers/prism/AbstractContent.js
@@ -1,8 +1,21 @@
 const core = require('gls-core-service');
 const Logger = core.utils.Logger;
 const Abstract = require('./Abstract');
+const env = require('../../data/env');
 
 class AbstractContent extends Abstract {
+    _extractTitle(content) {
+        return this._contentUtil.sanitize(content.headermssg);
+    }
+
+    _extractBodyFull(content) {
+        return this._contentUtil.sanitizePreview(content.bodymssg, env.GLS_CONTENT_PREVIEW_LENGTH);
+    }
+
+    _extractBodyPreview(content) {
+        return this._contentUtil.sanitize(content.bodymssg);
+    }
+
     _extractMetadata(content) {
         const raw = content.jsonmetadata;
 
