16be83536d9cfc033463ef38dd1b5ef724e2694e
Oleg <oleg.pav.m@gmail.com>
2019-03-06 14:43:12 +0300

    #97 #98 Envs + doc.

--- src/data/env.js
+++ src/data/env.js
@@ -1,6 +1,5 @@
 const env = process.env;
 
-// TODO Add max feed cache items
 module.exports = {
     NODE_OPTIONS: env.NODE_OPTIONS,
     GLS_MAX_FEED_LIMIT: Number(env.GLS_MAX_FEED_LIMIT) || 100,
@@ -9,4 +8,5 @@ module.exports = {
     GLS_CONTENT_PREVIEW_LENGTH: Number(env.GLS_CONTENT_PREVIEW_LENGTH) || 200,
     GLS_FEED_CACHE_INTERVAL: Number(env.GLS_FEED_CACHE_INTERVAL) || 300000,
     GLS_FEED_CACHE_TTL: Number(env.GLS_FEED_CACHE_TTL) || 28800000,
+    GLS_FEED_CACHE_MAX_ITEMS: Number(env.GLS_FEED_CACHE_MAX_ITEMS) || 10000,
 };
