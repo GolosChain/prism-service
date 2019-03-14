c7f5fe98db3ae2a55c7c0ec38d3e2a8b25a2227d
Oleg <oleg.pav.m@gmail.com>
2019-03-14 13:05:06 +0300

    #105 Skeleton.

---
+++ src/controllers/connector/Notify.js
@@ -0,0 +1,10 @@
+const core = require('gls-core-service');
+const BasicController = core.controllers.Basic;
+
+class Notify extends BasicController {
+    async getMeta() {
+        // TODO -
+    }
+}
+
+module.exports = Notify;
