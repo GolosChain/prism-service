ea4696dd1e623321b263c0ffa47041c9a96ca2e8
Oleg <oleg.pav.m@gmail.com>
2019-03-14 13:46:36 +0300

    #105 Doc + response skeleton/draft.

--- src/controllers/connector/Notify.js
+++ src/controllers/connector/Notify.js
@@ -2,7 +2,41 @@ const core = require('gls-core-service');
 const BasicController = core.controllers.Basic;
 
 class Notify extends BasicController {
-    async getMeta() {
+    async getMeta({ userId, communityId, postId, commentId }) {
+        const result = {};
+
+        if (userId) {
+            result.user = await this._getUserData();
+        }
+
+        if (communityId) {
+            result.community = await this._getCommunityData();
+        }
+
+        if (postId) {
+            result.post = await this._getPostData();
+        }
+
+        if (commentId) {
+            result.comment = await this._getCommentData();
+        }
+
+        return result;
+    }
+
+    async _getUserData() {
+        // TODO -
+    }
+
+    async _getCommunityData() {
+        // TODO -
+    }
+
+    async _getPostData() {
+        // TODO -
+    }
+
+    async _getCommentData() {
         // TODO -
     }
 }
