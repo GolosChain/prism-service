3605fff18584766c8a9cfe16957f430c6111d723
Oleg <oleg.pav.m@gmail.com>
2019-04-18 19:44:32 +0300

    #164 Add watch dog.

--- src/controllers/connector/Block.js
+++ src/controllers/connector/Block.js
@@ -1,5 +1,6 @@
 const core = require('gls-core-service');
 const BasicController = core.controllers.Basic;
+const env = require('../../data/env');
 
 class Block extends BasicController {
     constructor({ connector, prismService }) {
@@ -9,7 +10,7 @@ class Block extends BasicController {
     }
 
     waitForBlock({ blockNum }) {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             const currentBlockNum = this._prismService.getCurrentBlockNum();
 
             if (currentBlockNum >= blockNum) {
@@ -17,41 +18,76 @@ class Block extends BasicController {
                 return;
             }
 
-            this._startBlockNumChecker(blockNum, resolve);
+            const stopFlag = this._makeStopFlag();
+
+            this._startBlockNumChecker(blockNum, resolve, stopFlag);
+            this._setWatchDog(stopFlag, reject);
         });
     }
 
     waitForTransaction({ transactionId }) {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             if (this._prismService.hasRecentTransaction(transactionId)) {
                 resolve();
                 return;
             }
 
-            this._startTransactionIdChecker(transactionId, resolve);
+            const stopFlag = this._makeStopFlag();
+
+            this._startTransactionIdChecker(transactionId, resolve, stopFlag);
+            this._setWatchDog(stopFlag, reject);
         });
     }
 
-    _startBlockNumChecker(blockNum, resolve) {
+    _startBlockNumChecker(blockNum, resolve, stopFlag) {
         this._prismService.once('blockDone', currentBlockNum => {
+            if (this._isStopped(stopFlag)) {
+                return;
+            }
+
             if (currentBlockNum >= blockNum) {
                 resolve();
             } else {
-                // Avoid max call stack size error
-                setImmediate(() => this._startBlockNumChecker(blockNum, resolve));
+                // Avoid potential max call stack size error
+                setImmediate(() => this._startBlockNumChecker(blockNum, resolve, stopFlag));
             }
         });
     }
 
-    _startTransactionIdChecker(transactionId, resolve) {
+    _startTransactionIdChecker(transactionId, resolve, stopFlag) {
         this._prismService.once('transactionDone', currentTransactionId => {
+            if (this._isStopped(stopFlag)) {
+                return;
+            }
+
             if (currentTransactionId === transactionId) {
                 resolve();
             } else {
-                // Avoid max call stack size error
-                setImmediate(() => this._startTransactionIdChecker(transactionId, resolve));
+                // Avoid potential max call stack size error
+                setImmediate(() =>
+                    this._startTransactionIdChecker(transactionId, resolve, stopFlag)
+                );
             }
         });
+    }
+
+    _setWatchDog(stopFlag, reject) {
+        setTimeout(() => {
+            this._markStopped(stopFlag);
+            reject({ code: 408, message: 'Request timeout' });
+        }, env.GLS_MAX_WAIT_FOR_BLOCKCHAIN_TIMEOUT);
+    }
+
+    _makeStopFlag() {
+        return { stop: false };
+    }
+
+    _isStopped(stopFlag) {
+        return stopFlag.stop;
+    }
+
+    _markStopped(stopFlag) {
+        stopFlag.stop = true;
     }
 }
 
