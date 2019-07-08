dc3a6e635757d972fb9b9d918ca290efe2413c25
Ilya Lebedev <bacherko@gmail.com>
2019-07-08 14:56:14 +0300

    fix: update ServiceMeta

--- src/models/ServiceMeta.js
+++ src/models/ServiceMeta.js
@@ -6,20 +6,4 @@ module.exports = MongoDB.makeModel('ServiceMeta', {
         type: Boolean,
         default: false,
     },
-    lastBlockNum: {
-        type: Number,
-        default: 0,
-    },
-    lastBlockTime: {
-        type: Date,
-        default: null,
-    },
-    lastBlockSequence: {
-        type: Number,
-        default: 0,
-    },
-    inRevertProcess: {
-        type: Boolean,
-        default: false,
-    },
 });
