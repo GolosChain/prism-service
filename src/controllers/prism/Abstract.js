a063ff5de53e012c00e58ebc9d3c7e2660dc1ec0
Oleg <oleg.pav.m@gmail.com>
2019-07-08 17:54:07 +0300

    Connector hotfix.

--- src/controllers/prism/Abstract.js
+++ src/controllers/prism/Abstract.js
@@ -2,8 +2,8 @@ const core = require('gls-core-service');
 const BasicController = core.controllers.Basic;
 
 class Abstract extends BasicController {
-    constructor({ forkService } = {}, ...args) {
-        super(...args);
+    constructor({ forkService, ...args } = {}) {
+        super(args);
 
         this._forkService = forkService;
     }
